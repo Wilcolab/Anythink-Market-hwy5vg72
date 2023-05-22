d0c3c4771913949199d4855b6b3a7b4095788b6c
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-05-22 09:55:11 +0000

    bash file added

---
+++ backend/scripts/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if[$# -lt 1];then
+echo "no file is passed in args"
+exit 1
+fi
+log_file=$1
+if[! -f "$log_file"];then
+echo "invalid path"
+exit 1
+fi
+log_file_name=$(basename "$log_file")
+backup_file="/backup.tar.gz"
+backup_dir="/scripts/backend"
+tar -czf="$backup_file" -C / "$1"
+
+echo "task succesfull"
