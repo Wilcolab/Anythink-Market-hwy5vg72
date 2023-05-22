1b6bc57869cc80e62e44a00f1076073ea39b3400
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-05-22 22:59:10 +0000

    file added

--- backend/scripts/backup.sh
+++ backend/scripts/backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 log_file="$1"
-backup_file="backup.tar.gz"
-root_dir="/"
-tar -czf "$backup_file" "$log_file"
-mv "$backup_file" "$root_dir"
+
+
+tar -czf  "/backup.tar.gz" -C / "$log_file"
