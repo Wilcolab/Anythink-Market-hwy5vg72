3158b5dd8c2a079f8e21637ea7fd23b427a37410
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-17 22:56:18 +0000

    script modified 7

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 set -x
 set -e
-# Check if the correct number of arguments is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <input_csv_file>"
-    exit 1
-fi
 
 # Input CSV file provided as the first argument
 input_csv_file="$1"
 
-# Check if the input file exists
-if [ ! -f "$input_csv_file" ]; then
-    echo "Error: Input file not found: $input_csv_file"
-    exit 1
-fi
 
 # Output file to store the results
 output_txt_file="output_names.txt"
@@ -26,3 +16,4 @@ awk -F',' 'NR > 1 {print $3 " " $2}' "$input_csv_file" > "$output_txt_file"
 echo "Extraction completed. Results stored in $output_txt_file."
 cat backend/scripts/output_names.txt
 
+exit 0
