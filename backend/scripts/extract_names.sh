0945f2e797d7c513b05c4be14a7d7b7117e0d621
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-17 20:31:27 +0000

    shebang changed

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,25 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Usage: scripts
 set -x
+
+# Check if the correct number of arguments is provided
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <input_file>"
   exit 1
 fi
 
 input_file="$1"
-output_file="../../output_names.txt"
-
-
-# Check if the input file exists
-if [ ! -f "$input_file" ]; then
-  echo "Error: Input file '$input_file' not found."
-  exit 1
-fi
+output_file="/path/to/output_names.txt"  # Replace "/path/to" with the actual path to the desired output file location.
 
 # Function to extract names in the format "first name last name" from the input file
 extract_names() {
-  awk 'NR > 2 { print $3, $2 }' "$input_file" > "$output_file"
+  awk 'NR > 1 { print $3, $2 }' "$input_file" > "$output_file"
 }
 
 # Execute the extraction function
