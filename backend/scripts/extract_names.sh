41a18c77b0791539fb6e6c6d0cb30071db180ac9
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-17 22:07:30 +0000

    script modified 5

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -2,23 +2,32 @@
 
 # Check if the correct number of arguments is provided
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <input_csv_file>"
-  exit 1
+    echo "Usage: $0 <input_csv_file>"
+    exit 1
 fi
 
-# Input CSV file provided as an argument
-input_csv_file=$1
+# Input CSV file provided as the first argument
+input_csv_file="$1"
 
 # Check if the input file exists
 if [ ! -f "$input_csv_file" ]; then
-  echo "Error: Input file not found!"
-  exit 1
+    echo "Error: Input file not found: $input_csv_file"
+    exit 1
 fi
 
-# Output file to store the extracted names
+# Output file to store the results
 output_txt_file="output_names.txt"
 
-# Extract first and last names from the CSV file and log to console
-awk -F',' '{print $3, $2}' "$input_csv_file" | tee "$output_txt_file"
+# Remove output file if it already exists
+if [ -f "$output_txt_file" ]; then
+    rm "$output_txt_file"
+fi
 
-echo "Names extracted and stored in $output_txt_file."
+# Loop through the CSV file and extract first name + last name
+while IFS=',' read -r id last_name first_name email; do
+    full_name="$first_name $last_name"
+    echo "$full_name"
+    echo "$full_name" >> "$output_txt_file"
+done < "$input_csv_file"
+
+echo "Extraction completed. Results stored in $output_txt_file."
