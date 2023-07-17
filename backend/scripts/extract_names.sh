7eea56b9c1c3b5d2dcfd8f3c6f1d5df5a173aa6c
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-17 20:34:00 +0000

    code modifiedd

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -3,15 +3,8 @@
 # Usage: scripts
 set -x
 
-# Check if the correct number of arguments is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <input_file>"
-  exit 1
-fi
-
 input_file="$1"
-output_file="/path/to/output_names.txt"  # Replace "/path/to" with the actual path to the desired output file location.
-
+output_file="output_names.txt"  
 # Function to extract names in the format "first name last name" from the input file
 extract_names() {
   awk 'NR > 1 { print $3, $2 }' "$input_file" > "$output_file"
