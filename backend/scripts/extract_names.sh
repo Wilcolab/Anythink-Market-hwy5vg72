101fb47e433ef7b02d01e755599730d6f528b96c
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-17 22:36:24 +0000

    script modified 6

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 
 # Check if the correct number of arguments is provided
 if [ $# -ne 1 ]; then
@@ -16,8 +15,11 @@ if [ ! -f "$input_csv_file" ]; then
     exit 1
 fi
 
+# Get the absolute path to the input CSV file
+input_csv_file="$(realpath "$input_csv_file")"
+
 # Output file to store the results
-output_txt_file="output_names.txt"
+output_txt_file="$(dirname "$input_csv_file")/output_names.txt"
 
 # Remove output file if it already exists
 if [ -f "$output_txt_file" ]; then
@@ -31,4 +33,12 @@ tail -n +2 "$input_csv_file" | while IFS=',' read -r id last_name first_name ema
     echo "$full_name" >> "$output_txt_file"
 done
 
+# Check if the output file was successfully created
+if [ ! -f "$output_txt_file" ]; then
+    echo "Error: Failed to create the output file: $output_txt_file"
+    exit 1
+fi
+
+# Display a success message and the contents of the output file
 echo "Extraction completed. Results stored in $output_txt_file."
+cat "$output_txt_file"
