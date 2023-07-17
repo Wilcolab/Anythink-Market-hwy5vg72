40d1c2481da1784390b9bc4003ce978e3edcfd42
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-17 21:41:23 +0000

    script modified

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -3,11 +3,17 @@
 set -x
 set -e
 
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <input_file>"
+  exit 1
+fi
+
 input_file="$1"
 output_file="output_names.txt"  
-# Function to extract names in the format "first name last name" from the input file
+
+# Function to extract names in the format "first name last name" from the spreadsheet input file
 extract_names() {
-  awk 'NR > 1 { print $3, $2 }' "$input_file" > "$output_file"
+ awk -F ',' 'NR > 1 { gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2); gsub(/^[[:space:]]+|[[:space:]]+$/, "", $3); print $3, $2; }' "$input_file" | tee "$output_file"
 }
 
 # Execute the extraction function
