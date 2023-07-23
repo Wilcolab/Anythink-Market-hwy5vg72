ec01fa81524b98c7fa046d864d20533d6ccdd0b2
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2023-07-23 06:35:17 +0000

    added script

--- backend/scripts/backup.sh
+++ backend/scripts/backup.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+hugo
 set -x
 set -e 
 echo "hello"
 file=$1
 
-tar czf backup.tar.gz $file
+tar czf ~/backup.tar.gz "$file"
+
+echo "Backup created successfully: ~/backup.tar.gz"
+exit 0
