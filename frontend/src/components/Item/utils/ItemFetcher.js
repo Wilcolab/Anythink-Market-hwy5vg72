b8b0d9d4788d94c16c1573137151892babb8e5d1
WilcoApp <hello@wilco.gg>
2023-09-11 03:15:30 +0000

    Funnel Drop

--- frontend/src/components/Item/utils/ItemFetcher.js
+++ frontend/src/components/Item/utils/ItemFetcher.js
@@ -1,15 +1,8 @@
 import agent from "../../../agent";
 
-
 export async function getItemAndComments(id) {
-  // Start both asynchronous requests simultaneously
-  const [itemPromise, commentsPromise] = [
-    agent.Items.get(id),
-    agent.Comments.forItem(id),
-  ];
-
-  // Wait for both promises to resolve using Promise.all
-  const [item, comments] = await Promise.all([itemPromise, commentsPromise]);
+  const item = await agent.Items.get(id);
+  const comments = await agent.Comments.forItem(id);
 
   return [item, comments];
 }
