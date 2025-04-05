cce8073aac6280d02b7f65941c0eb071bfd1d179
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2025-04-05 09:41:48 +0000

    changes

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/", greet)
+	router.GET("/items",item)
 	router.HEAD("/healthcheck", healthcheck)
 
 	router.Run()
 }
+type Item struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
+func item(c *gin.Context){
+	
+	names := []string{"Galactic Goggles","Meteor Muffins","Alien Antenna Kit","Starlight Lantern","Quantum Quill"}
+	items:= []Item{}
+	for i, name := range names{
+		items=append(items,Item{ID: i , Name : name})
+	}
+	fmt.Println("************************************")
+	fmt.Print(items)
+	fmt.Println("************************************")
+
+	c.JSON(http.StatusOK, items)
+}
 func greet(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
 }
