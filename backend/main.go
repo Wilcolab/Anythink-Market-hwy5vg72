4fd3492c32094d1ee03633a0ca3670491b1a2116
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2025-04-05 18:27:08 +0000

    added

--- backend/main.go
+++ backend/main.go
@@ -1,61 +1,8 @@
-package main
+# Make sure you're on the correct branch
+git checkout sumit
 
-import (
-	"net/http"
-	"github.com/gin-gonic/gin"
-	"fmt"
-)
+# Fetch the latest changes from origin
+git fetch origin
 
-func main() {
-	router := gin.Default()
-	router.GET("/", greet)
-	router.GET("/items",item)
-	// router.POST("/items",additem)
-	router.HEAD("/healthcheck", healthcheck)
-
-	router.Run()
-}
-
-type Item struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-var names = []string{"Galactic Goggles","Meteor Muffins","Alien Antenna Kit","Starlight Lantern","Quantum Quill"}
-
-
-type NewItem struct{
-	Name string `json:"name"`
-}
-
-func additem(c *gin.Context){
-var newItem  NewItem
-if err := c.BindJSON(&newItem); err!=nil{
-	c.JSON(http.StatusBadRequest,gin.H{"error": "invalid input"})
-	return
-}
-var id_item=len(names)+1
-names=append(names,newItem.Name)
-items=append(items,Item{ID: id_item, Name : newItem.Name})
-c.JSON(http.StatusOK,  Item{ID: id_item, Name: newItem.Name})
-
-}
-func item(c *gin.Context){
-	items = []Item{}
-	for i, name := range names{
-		items=append(items,Item{ID: i+1 , Name : name})
-	}
-	fmt.Println("************************************")
-	fmt.Print(items)
-	fmt.Println("************************************")
-
-	c.JSON(http.StatusOK, items)
-}
-func greet(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
-}
-
-func healthcheck(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-	})
-}
+# Rebase your branch onto the latest main
+git rebase origin/main
