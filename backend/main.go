3f9b0de65d262e29dc2fc4a656945f9687929f10
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2025-04-05 20:04:53 +0000

    files added

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", greet)
 	router.GET("/items", items)
+	router.POST("/items", additems)
 	router.HEAD("/healthcheck", healthcheck)
 
 	router.Run()
@@ -27,9 +28,25 @@ func items(c *gin.Context){
 	}
 	c.JSON(http.StatusOK,item )
 }
+type NewItem struct{
+	NAME string  `json:"name"`
+}
+func additems(c *gin.Context){
+	var newItem NewItem
+	// item := []Items{} 
+	if err := c.BindJSON(&newItem);err != nil{
+		return
+	}
+	var item_id = len(Listitems)+1
+	Listitems=append(Listitems,Items{ID: item_id, NAME: newItem.NAME,})
+	c.JSON(http.StatusOK, Items{ID: item_id, NAME: newItem.NAME,})
+	
+
+}
 func greet(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
 }
+
 
 func healthcheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
