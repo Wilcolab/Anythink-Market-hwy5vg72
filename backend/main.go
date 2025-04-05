ba96e50452ccf0c67a668a96ae3bf0449328758d
Sumit Dethe <91131672+sumitdethe27@users.noreply.github.com>
2025-04-05 13:04:31 +0000

    files added

--- backend/main.go
+++ backend/main.go
@@ -10,19 +10,37 @@ func main() {
 	router := gin.Default()
 	router.GET("/", greet)
 	router.GET("/items",item)
+	router.POST("/items",additem)
 	router.HEAD("/healthcheck", healthcheck)
 
 	router.Run()
 }
+
 type Item struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
+var names = []string{"Galactic Goggles","Meteor Muffins","Alien Antenna Kit","Starlight Lantern","Quantum Quill"}
+var items= []Item{}
 
+type NewItem struct{
+	Name string `json:"name"`
+}
+
+func additem(c *gin.Context){
+var newItem  NewItem
+if err := c.BindJSON(&newItem); err!=nil{
+	c.JSON(http.StatusBadRequest,gin.H{"error": "invalid input"})
+	return
+}
+var id_item=len(names)+1
+names=append(names,newItem.Name)
+items=append(items,Item{ID: id_item, Name : newItem.Name})
+c.JSON(http.StatusOK,  Item{ID: id_item, Name: newItem.Name})
+
+}
 func item(c *gin.Context){
 	
-	names := []string{"Galactic Goggles","Meteor Muffins","Alien Antenna Kit","Starlight Lantern","Quantum Quill"}
-	items:= []Item{}
 	for i, name := range names{
 		items=append(items,Item{ID: i+1 , Name : name})
 	}
